878f39e1584ed2c4f557831375154b0110fad8ac
Marta <martasteblik@wp.pl>
2016-04-25 12:42:51 +0200

    Kolokwium

---
+++ KolokwiumTests/DivideAndConquerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zaliczenie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zaliczenie.Tests
+{
+    [TestClass()]
+    public class DivideAndConquerTests
+    {
+        [TestMethod()]
+        public void DivideTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ConquerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CombineTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
